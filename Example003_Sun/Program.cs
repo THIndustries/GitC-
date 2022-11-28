e4d4a2929921dd029adfa5c19a85efb07079009e
MyName <dragon-negativ@yandex.ru>
2022-11-28 16:55:35 +0300

    добивил суммирование рандомных чисел в Example003

--- Example003_Sun/Program.cs
+++ Example003_Sun/Program.cs
@@ -1,4 +1,8 @@
-﻿int numA = 36;
-int numB = 5;
+﻿int numA = new Random().Next(1, 10); //1... 9
+Console.Write(numA);
+Console.Write(" + ");
+int numB = new Random().Next(1, 10);
+Console.Write(numB);
+Console.Write(" = ");
 int sum = numA + numB;
-Console.WriteLine(sum)
+Console.Write(sum);
