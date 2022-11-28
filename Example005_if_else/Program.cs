50e58792c36707c15eaa3cdf4fc0ce0159c3157b
MyName <dragon-negativ@yandex.ru>
2022-11-28 17:53:59 +0300

    добавил программку Example005

---
+++ Example005_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша") //интересная операция ToLower(), как я понял, помогает игнорировать регистр
+{
+    Console.WriteLine("Да это же сама Маша");
+}
+else
+{
+    Console.Write("Привет ");
+    Console.WriteLine(username);
+}
