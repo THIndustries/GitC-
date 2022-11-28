e6d4acb37722a1091fae722933065b3c048a1382
MyName <dragon-negativ@yandex.ru>
2022-11-28 14:49:37 +0300

    добавил gitignore

---
+++ Example002_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿// тут будет ввод имени юзера. обрати внимание, что "Console.WriteLine" - выводит текст с новой строки.
+Console.Write("Введите ваше имя");
+string username = Console.ReadLine();
+Console.Write("Привет, ");
+Console.Write(username);
+
+// попробуем команду Console.Write - тогда текст выйдет в одну строку
